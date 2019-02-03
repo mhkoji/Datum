864963d4147a4f9700cba1e7daac22928c77d753
mhkoji <mhkoji2+git@gmail.com>
2019-02-03 22:16:23 +0900

    Fix

--- datum-app/web/src/cl/web.lisp
+++ datum-app/web/src/cl/web.lisp
@@ -14,8 +14,7 @@
   (let ((app (make-instance 'ningle:<app>)))
     (datum.app.web.route.api:bind-album app conf)
     (datum.app.web.route.api:bind-image app conf)
-    (datum.app.web.route.asset:bind-html app)
     (datum.app.web.route.asset:bind-resources
-     app
-     (namestring *default-pathname-defaults*))
+     app (namestring *default-pathname-defaults*))
+    (datum.app.web.route.asset:bind-html app)
     (setq *handler* (clack:clackup app :port port))))
