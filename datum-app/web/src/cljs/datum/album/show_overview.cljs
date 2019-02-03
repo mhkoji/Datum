07998306d7c48021d93dd4d21bf22a74561b6184
mhkoji <mhkoji2+git@gmail.com>
2019-02-03 23:08:41 +0900

    datum-web: add album/show_overview

---
+++ datum-app/web/src/cljs/datum/album/show_overview.cljs
@@ -0,0 +1,24 @@
+(ns datum.album.show-overview
+  (:require [cljs.core.async :refer [go <! timeout]]))
+
+(defprotocol Api
+  (name [this album-id k])
+  (pictures [this album-id k]))
+
+(defprotocol Transaction
+  (get-state [this])
+  (update-state [this f]))
+
+
+(defrecord State [album-id name pictures])
+
+(defn execute [transaction api]
+  (let [state (get-state transaction)]
+    ;; Name
+    (when (not (state :name))
+      (name api (state :album-id) (fn [name]
+       (update-state transaction #(assoc % :name name)))))
+    ;; Pictures
+    (when (not (state :pictures))
+      (pictures api (state :album-id) (fn [pictures]
+       (update-state transaction #(assoc % :pictures images)))))))
