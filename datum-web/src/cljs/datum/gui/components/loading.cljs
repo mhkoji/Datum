d91933006dfc61c072a3ed17fd05425ee31bf4d0
mhkoji <mhkoji2+git@gmail.com>
2019-10-20 02:26:14 +0900

    datum-web: make spinner bigger

--- datum-web/src/cljs/datum/gui/components/loading.cljs
+++ datum-web/src/cljs/datum/gui/components/loading.cljs
@@ -2,7 +2,7 @@
 
 (defn spinner []
   [:div {:class "d-flex justify-content-center"}
-   [:div {:class "spinner-border"
+   [:div {:class "spinner-border m-5"
           :style {:width "3rem" :height "3rem"}
           :role "status"}
     [:span {:class "sr-only"} "Loading..."]]])
