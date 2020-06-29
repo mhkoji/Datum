7ed94e8e21e925464a895a31121da82be11f9a7d
mhkoji <mhkoji2+git@gmail.com>
2020-06-29 21:55:29 +0900

    datum: use native query

--- datum/src/db/mito/album.lisp
+++ datum/src/db/mito/album.lisp
@@ -1,122 +1,148 @@
-(defpackage :datum.db.mito.album
-  (:use :cl :datum.album.db)
-  (:export :album
-           :album-thumbnail)
-  (:import-from :dbi.driver
-                :<dbi-connection>))
-(in-package :datum.db.mito.album)
+(in-package :datum.db.mito)
 
-(defmacro conc-strings (&rest strings)
-  `(concatenate 'string ,@strings))
+(defmethod datum.album.db:insert-album-rows ((db <dbi-connection>)
+                                             (rows list))
+  (when rows
+    (let ((sql (conc-strings
+                "INSERT INTO album"
+                " (album_id, name, updated_at)"
+                " VALUES"
+                (format nil "~{~A~^,~}"
+                        (loop repeat (length rows) collect "(?,?,?)")))))
+      (execute db
+               sql
+               (alexandria:mappend
+                (lambda (row)
+                  (list (datum.id:to-string
+                         (datum.album.db:album-row-id row))
+                        (datum.album.db:album-row-name row)
+                        (format-datetime
+                         (datum.album.db:album-row-updated-at row))))
+                rows)))))
 
-(defun query (db sql params)
-  (dbi:fetch-all (apply #'dbi:execute (dbi:prepare db sql) params)))
-
-(defclass album (datum.db.mito:listed)
-  ((album-id :col-type (:varchar 256)
-             :accessor album-id)
-   (name :col-type (:varchar 256)
-         :accessor album-name)
-   (updated-at :col-type :timestamp
-               :accessor album-updated-at))
-  (:metaclass mito:dao-table-class)
-  (:record-timestamps nil))
-
-(defmethod insert-album-rows ((db <dbi-connection>)
-                              (rows list))
-  (dolist (row rows)
-    (mito:create-dao 'album
-                     :album-id (datum.id:to-string (album-row-id row))
-                     :name (album-row-name row)
-                     :updated-at (album-row-updated-at row))))
-
-(defmethod select-album-rows ((db <dbi-connection>)
-                              (album-ids list))
+(defmethod datum.album.db:select-album-rows ((db <dbi-connection>)
+                                             (album-ids list))
   (when album-ids
     (let ((sql (conc-strings
                 "SELECT *"
                 " FROM"
                 "  album"
                 " WHERE"
-                "  album_id in ("
+                "  album_id in"
+                " ("
                 (format nil "~{~A~^,~}"
                         (loop repeat (length album-ids) collect "?"))
-                ")")))
+                " )")))
       (let ((plist-rows
-             (query db sql
-                    (mapcar #'datum.id:to-string album-ids))))
+             (query db sql (mapcar #'datum.id:to-string album-ids))))
         (mapcar (lambda (plist)
-                  (make-album-row
+                  (datum.album.db:make-album-row
                    :id (datum.id:from-string (getf plist :|album_id|))
                    :name (getf plist :|name|)
-                   :updated-at (local-time:parse-timestring
-                                (getf plist :|updated_at|)
-                                :date-time-separator #\Space)))
+                   :updated-at (parse-datetime
+                                (getf plist :|updated_at|))))
                 plist-rows)))))
 
-(defmethod select-album-ids ((db <dbi-connection>)
-                             offset count)
-  (let ((sql (conc-strings
-              "SELECT album_id"
-              " FROM"
-              "  album"
-              " ORDER BY"
-              "  updated_at DESC"
-              " LIMIT"
-              "  ?, ?")))
-    (let ((plist-rows
-           (query db sql (list offset count))))
-      (mapcar (lambda (plist)
-                (datum.id:from-string (getf plist :|album_id|)))
-              plist-rows))))
+(labels ((get-album-ids (plist-rows)
+           (mapcar (lambda (plist)
+                     (datum.id:from-string (getf plist :|album_id|)))
+                   plist-rows)))
+  (defmethod datum.album.db:select-album-ids ((db <dbi-connection>)
+                                              offset count)
+    (let ((sql (conc-strings
+                "SELECT album_id"
+                " FROM"
+                "  album"
+                " ORDER BY"
+                "  updated_at DESC"
+                " LIMIT"
+                "  ?, ?")))
+      (get-album-ids
+       (query db sql (list offset count)))))
 
-(defmethod select-album-ids-by-like ((db <dbi-connection>)
-                                     (name string))
-  (mapcar (alexandria:compose #'datum.id:from-string #'album-id)
-          (mito:select-dao 'album
-            (sxql:where (:like :name (format nil "%~A%" name)))
-            (sxql:order-by (:desc :updated_at))
-            (sxql:limit 50))))
+  (defmethod datum.album.db:select-album-ids-by-like ((db <dbi-connection>)
+                                                      (name string))
+    (let ((sql (conc-strings
+                "SELECT album_id"
+                " FROM"
+                "  album"
+                " WHERE"
+                "  name like ?"
+                " ORDER BY"
+                "  updated_at DESC"
+                " LIMIT"
+                "  50")))
+      (get-album-ids
+       (query db sql (list (format nil "%~A%" name)))))))
 
+(defmethod datum.album.db:delete-album-rows ((db <dbi-connection>)
+                                             (album-ids list))
+  (when album-ids
+    (let ((sql (conc-strings
+                "DELETE"
+                " FROM"
+                "  album"
+                " WHERE"
+                "  album_id in"
+                " ("
+                (format nil "~{~A~^,~}"
+                        (loop repeat (length album-ids) collect "?"))
+                " )")))
+      (execute db sql (mapcar #'datum.id:to-string album-ids)))))
 
-(defmethod delete-album-rows ((db <dbi-connection>) (album-ids list))
-  (dolist (album-id album-ids)
-    (mito:delete-by-values 'album
-                           :album-id (datum.id:to-string album-id))))
+(defmethod datum.album.db:insert-album-thumbnail-rows ((db <dbi-connection>)
+                                                       (rows list))
+  (when rows
+    (let ((sql (conc-strings
+                "INSERT INTO album_thumbnail"
+                " (album_id, thumbnail_id)"
+                " VALUES"
+                (format nil "~{~A~^,~}"
+                        (loop repeat (length rows) collect "(?,?)"))))
+          (params
+           (alexandria:mappend
+            (lambda (row)
+              (list (datum.id:to-string
+                     (datum.album.db:album-thumbnail-row-album-id row))
+                    (datum.id:to-string
+                     (datum.album.db:album-thumbnail-row-thumbnail-id row))))
+            rows)))
+      (execute db sql params))))
 
+(defmethod datum.album.db:select-album-thumbnail-rows ((db <dbi-connection>)
+                                                       (album-ids list))
+  (when album-ids
+    (let ((sql (conc-strings
+                "SELECT *"
+                " FROM"
+                "  album_thumbnail"
+                " WHERE"
+                "  album_id in"
+                " ("
+                (format nil "~{~A~^,~}"
+                        (loop repeat (length album-ids) collect "?"))
+                " )")))
+      (let ((plist-rows
+             (query db sql (mapcar #'datum.id:to-string album-ids))))
+        (mapcar (lambda (plist)
+                  (datum.album.db:make-album-thumbnail-row
+                   :album-id
+                   (datum.id:from-string (getf plist :|album_id|))
+                   :thumbnail-id
+                   (datum.id:from-string (getf plist :|thumbnail_id|))))
+                plist-rows)))))
 
-(defclass album-thumbnail (datum.db.mito:listed)
-  ((album-id :col-type (:varchar 256)
-             :accessor album-id)
-   (thumbnail-id :col-type (:varchar 256)
-                 :accessor album-thumbnail-id))
-  (:metaclass mito:dao-table-class))
-
-(defmethod insert-album-thumbnail-rows ((db <dbi-connection>)
-                                        (rows list))
-  (dolist (row rows)
-    (mito:create-dao 'album-thumbnail
-                     :album-id
-                     (datum.id:to-string
-                      (album-thumbnail-row-album-id row))
-                     :thumbnail-id
-                     (datum.id:to-string
-                      (album-thumbnail-row-thumbnail-id row)))))
-
-(defmethod select-album-thumbnail-rows ((db <dbi-connection>)
-                                        (album-ids list))
-  (let ((objects (mito:select-dao 'album-thumbnail
-                   (sxql:where (:in :album-id album-ids)))))
-    (mapcar (lambda (obj)
-              (make-album-thumbnail-row
-               :album-id
-               (datum.id:from-string (album-id obj))
-               :thumbnail-id
-               (datum.id:from-string (album-thumbnail-id obj))))
-            objects)))
-
-(defmethod delete-album-thumbnail-rows ((db <dbi-connection>)
-                                        (album-ids list))
-  (dolist (album-id album-ids)
-    (mito:delete-by-values 'album-thumbnail
-                           :album-id (datum.id:to-string album-id))))
+(defmethod datum.album.db:delete-album-thumbnail-rows ((db <dbi-connection>)
+                                                       (album-ids list))
+  (when album-ids
+    (let ((sql (conc-strings
+                "DELETE"
+                " FROM"
+                "  album_thumbnail"
+                " WHERE"
+                "  album_id in"
+                " ("
+                (format nil "~{~A~^,~}"
+                        (loop repeat (length album-ids) collect "?"))
+                " )")))
+      (execute db sql (mapcar #'datum.id:to-string album-ids)))))
--- datum/src/db/mito/mito.lisp
+++ datum/src/db/mito/mito.lisp
@@ -1,7 +1,9 @@
 (defpackage :datum.db.mito
   (:use :cl :datum.db)
   (:export :mito-factory
-           :listed))
+           :listed)
+  (:import-from :dbi.driver
+                :<dbi-connection>))
 (in-package :datum.db.mito)
 
 (defclass mito-factory ()
@@ -27,3 +29,44 @@
   (dbi:with-transaction db
     (let ((mito:*connection* db))
       (funcall callback db))))
+
+
+(defmacro conc-strings (&rest strings)
+  `(concatenate 'string ,@strings))
+
+(defun format-datetime (datetime)
+  (local-time:format-timestring nil datetime
+   :format '((:year 4) #\- (:month 2) #\- (:day 2)
+             #\Space
+             (:hour 2) #\: (:min 2) #\: (:sec 2) #\. (:nsec))))
+
+(defun parse-datetime (string)
+  (local-time:parse-timestring string :date-time-separator #\Space))
+
+
+(defun execute (db sql params)
+  (apply #'dbi:execute (dbi:prepare db sql) params)
+  (values))
+
+(defun query (db sql params)
+  (dbi:fetch-all (apply #'dbi:execute (dbi:prepare db sql) params)))
+
+
+;;;
+
+(defclass album (listed)
+  ((album-id :col-type (:varchar 256)
+             :accessor album-id)
+   (name :col-type (:varchar 256)
+         :accessor album-name)
+   (updated-at :col-type :timestamp
+               :accessor album-updated-at))
+  (:metaclass mito:dao-table-class)
+  (:record-timestamps nil))
+
+(defclass album-thumbnail (listed)
+  ((album-id :col-type (:varchar 256)
+             :accessor album-id)
+   (thumbnail-id :col-type (:varchar 256)
+                 :accessor album-thumbnail-id))
+  (:metaclass mito:dao-table-class))
